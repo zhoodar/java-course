c402397f4c852c8b09b4498e8d1dc3ecffc671a0
ZHoodar <ajoodar@gmail.com>
2016-04-24 20:10:04 +0600

    Added 4 methods  getNumberofClients, reNameClient, toPrint, toString

--- Clinic/src/main/java/kg/djedai/app/clinic/Clinic.java
+++ Clinic/src/main/java/kg/djedai/app/clinic/Clinic.java
@@ -23,6 +23,7 @@ package kg.djedai.app.clinic;
     public Client[] getClients(){
         return this.clients;
     }
+
 	/**
 	 * Добавить клиента.
 	 * @param position Позиция в массиве 
@@ -39,7 +40,7 @@ package kg.djedai.app.clinic;
 	public Client[] findClientByPetName(final String name){
 		int position=0;
 		for(int j = 0; j < this.clients.length; j++) {
-            if (this.clients[j] != null && this.clients[j].getPet() != null && this.clients[j].getPet().getName().equals(name))
+            if (this.clients[j] != null && this.clients[j].getPet() != null && this.clients[j].getPetName().equals(name))
                 position=j;
         }
         return new Client[]{this.clients[position]};
@@ -55,13 +56,14 @@ package kg.djedai.app.clinic;
         for(int j=0; j<this.clients.length; j++){
             if( this.clients[j] != null && this.clients[j].getId().equals(id) ){
                 position=j;
+                break;
             }
         }
         return new Client[]{this.clients[position]};
     }
 
     /**
-     * Удалить клиента если есть такой id клиет
+     * Удалить клиента если есть клиент c такой id
      * @param id Имя клиента
      * @return boolean  true or false
      */
@@ -71,11 +73,76 @@ package kg.djedai.app.clinic;
             if(this.clients[j]!=null&& this.clients[j].getId().equals(id)){
                 this.clients[j]=null;
                 isRemoved=true;
+                break;
             }
         }
         return isRemoved;
     }
 
+    /**
+     * Переименовать имени клиента
+     * @param oldName старый имя
+     * @param newName новый имя
+     * @return boolean true or false
+     */
+    public boolean reNameClientName(final String oldName, final String newName){
+        boolean isRenamed = false;
+        for(int j =0; j<this.clients.length; j++) {
+            if(this.clients[j] != null && this.clients[j].getId().equals(oldName)) {
+                Client reNamed = new Client(newName,this.clients[j].getPet());
+                clients[j]=reNamed;
+                isRenamed= true;
+                break;
+            }
+        }
+        return isRenamed;
+    }
 
+    /**
+     * Печатать на экран введеного клитна(ов)
+     * @param clients массив клиентов
+     */
+    public void  toPrint(Client[] clients){
+        for(Client client : clients){
+            if(client !=null) {
+                Pet pet = client.getPet();
+                System.out.print("Client: " + client.getId());
+                if(pet != null){
+                    System.out.println(", pet: "+pet.getName());
+                }
+                else System.out.println();
+            }
+        }
+    }
 
+    /**
+     * Переобразование из массива в строку
+     * @param clients массив
+     * @return строка
+     */
+    public String toString(Client[] clients){
+        String result = "";
+        for(Client client : clients){
+            if(client !=null) {
+                Pet pet = client.getPet();
+                if(pet != null){
+                    result="Client: " + client.getId()+", pet: "+pet.getName();
+                }
+                else result = "";
+            }
+        }
+        return result;
+    }
+
+    /**
+     * для нахождение позици в массиве
+     * @return int
+     */
+    public int getNumberOfClients(){
+        int counter=0;
+        for(int j =0;j<this.clients.length;j++){
+            counter++;
+        }
+        return counter;
+    }
  }
