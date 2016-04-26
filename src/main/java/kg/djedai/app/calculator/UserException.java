e55ef4437689fa37e14e56f4e63e9ee252cbbd38
ZHoodar <ajoodar@gmail.com>
2016-04-26 12:28:41 +0600

    app calculator: intial commit

---
+++ src/main/java/kg/djedai/app/calculator/UserException.java
@@ -0,0 +1,12 @@
+package kg.djedai.app.calculator;
+
+/**
+ * Class UserException to handle exceptions
+ */
+public class UserException extends Exception {
+
+    public UserException(String s) {
+		super(s);
+    }
+
+}
