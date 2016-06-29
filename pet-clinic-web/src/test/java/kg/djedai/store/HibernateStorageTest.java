35397eb89f522c7e53756f1e54dc0809d0e931bb
ZHoodar <ajoodar@gmail.com>
2016-06-29 15:29:27 +0600

    #21Hibernate, the HibernateStorage class is covered with 96% of JUnit tests

--- pet-clinic-web/src/test/java/kg/djedai/store/HibernateStorageTest.java
+++ pet-clinic-web/src/test/java/kg/djedai/store/HibernateStorageTest.java
@@ -19,11 +19,10 @@ public class HibernateStorageTest {
     @Test
     public void testCreate(){
         final HibernateStorage storage = new HibernateStorage();
-        final String id = "55XXCCHH";
-        storage.addClient(new ClientModel(id, "hibernate"));
+        final String id = storage.generateId();
+        storage.addClient(new ClientModel(id, "Amanda"));
         final ClientModel client = storage.getClientById(id);
         assertEquals(id, client.getId());
-        assertEquals(1, storage.findByFullName("hibernate").size());
         storage.deleteClient(id);
         assertNull(storage.getClientById(id));
         storage.close();
@@ -53,20 +52,49 @@ public class HibernateStorageTest {
     public void testSearch(){
         final HibernateStorage storage = new HibernateStorage();
         final String id = storage.generateId();
-        String clientName = "HiberClient";
-        String petName = "HiberCat";
+        String clientName = "Peter Cllk";
+        String petName = "testPuppy";
         Pet cat = new Cat(petName);
         storage.addClient(new ClientModel(id,clientName));
         storage.addPetToClient(cat,id);
 
         assertEquals(1,storage.findByFullName(petName).size());
-        assertEquals(1,storage.findByContain("ber").size());
+        assertEquals(1,storage.findByContain("llk").size());
         assertEquals(1,storage.findByFullName(clientName).size());
 
         storage.deleteClient(id);
         storage.close();
-
-
     }
+
+    @Test
+    public void testDelete(){
+        final HibernateStorage storage = new HibernateStorage();
+        final String iD = storage.generateId();
+        String clientName = "Peter Ku";
+        String petName = "Puppy";
+        Pet cat = new Cat(petName);
+        storage.addClient(new ClientModel(iD,clientName));
+        storage.addPetToClient(cat,iD);
+        String id = storage.getClients().iterator().next().getId();
+        int size = storage.getClients().size();
+        assertEquals(size,storage.getClients().size());
+        storage.deleteClient(id);
+        assertFalse(size==storage.getClients().size());
+        storage.close();
+    }
+
+    @Test
+    public void testEdit(){
+        final HibernateStorage storage = new HibernateStorage();
+        ClientModel client = storage.getClients().iterator().next();
+        String id = client.getId();
+        String editedName = "EditedName";
+        assertFalse(client.getNameClient().equals(editedName));
+        client.setNameClient(editedName);
+        storage.editClient(client);
+        assertTrue(storage.getClientById(id).getNameClient().equals(editedName));
+        storage.close();
+    }
+
 
 }
